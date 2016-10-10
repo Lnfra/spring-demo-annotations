c0b3033c1758b1db5db5ebaa1db57bc50ac0d160
Angeline Tan <atan009@gmail.com>
2016-10-11 03:31:14 +0800

    Field injection by qualifier. Created RandomFortune Service

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,11 +1,14 @@
 package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
   
+  @Autowired
+  @Qualifier("randomFortuneService")
   private FortuneService fortuneService;
 
   //define a default constructor
@@ -14,11 +17,11 @@ public class TennisCoach implements Coach {
   }
   
   //define a setter
-  @Autowired
-  public void doSomeCrazyStuff( FortuneService theFortuneService){
-    System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
-    fortuneService = theFortuneService;
-  }
+//  @Autowired
+//  public void doSomeCrazyStuff( FortuneService theFortuneService){
+//    System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
+//    fortuneService = theFortuneService;
+//  }
   
 //  @Autowired
 //  public TennisCoach( FortuneService theFortuneService ){
