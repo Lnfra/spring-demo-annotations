bb1b58b51be852057c4252748a4594625f82bd9b
Angeline Tan <atan009@gmail.com>
2016-10-13 18:16:17 +0800

    Injecting email and team values from properties file via java code in sport config

--- src/com/luv2code/springdemo/SportConfig.java
+++ src/com/luv2code/springdemo/SportConfig.java
@@ -3,11 +3,21 @@ package com.luv2code.springdemo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 //@ComponentScan("com.luv2code.springdemo")
+@PropertySource("classpath:sport.properties")
 public class SportConfig {
 
+  //add support to resolve ${..} properties
+  @Bean
+  public static PropertySourcesPlaceholderConfigurer 
+                 properySourcesPlaceholderConfigurer() {
+    return new PropertySourcesPlaceholderConfigurer();
+  }
+  
   //define bean for our sad fortune service
   @Bean
   public FortuneService sadFortuneService(){
